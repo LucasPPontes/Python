c63c7a2f9d91af302eff9953af56340aba90c905
LucasPPontes <lucaspontes360@gmail.com>
2023-02-16 17:31:50 -0300

    done

--- github.py
+++ github.py
@@ -1,7 +1,10 @@
 import os
+import time
 
 os.system("git status")
 os.system("git add .")
 os.system("git commit -m done ")
 os.system("git status")
 os.system("git push -uf origin main")
+
+time.sleep(10)
